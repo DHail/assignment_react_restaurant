b5931359230ae49ec058649e6060dd8f24b7a9a5
markmarkyesyes <markhahn545@gmail.com>
2017-05-01 12:45:07 -0500

    set up boilerplate

---
+++ restaurant-website/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import Jumbotron from "./Jumbotron";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header" />
+        <Jumbotron />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ restaurant-website/src/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div class="jumbotron">
+      <h1>Beyonce's BabyBack'sRibs</h1>
+      <p>Get you some.</p>
+
+    </div>
+  );
+};
+
+export default Jumbotron;
